c052de14860d9235f40d1bf73628974575efbfa8
jungwonse <vvv909vvv@gmail.com>
2023-04-09 10:03:48 +0900

    1차 코드 반영

--- lib/screens/ai/ai_screen.dart
+++ lib/screens/ai/ai_screen.dart
@@ -1,10 +1,110 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'dart:convert';
 
 class AiScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('질병 진단'),
+    return MaterialApp(
+      title: 'AI 질병 진단 페이지',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'AI 질병 진단 페이지'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late File _imageFile;
+  final picker = ImagePicker();
+
+  _MyHomePageState() {
+    _imageFile = File(''); // 파일 초기화
+  }
+
+  Future<void> _getImage(ImageSource source) async {
+    final pickedFile = await picker.getImage(source: source);
+
+    setState(() {
+      if (pickedFile != null) {
+        _imageFile = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future<void> _uploadImage(File image) async {
+    // 주소 변경해야 함
+    final url = 'http://5b03-34-126-162-145.ngrok.io';
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'image/jpeg'},
+      body: await image.readAsBytes(),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(utf8.decode(response.bodyBytes));
+      String className = data['class_name'];
+      print('질병 확인: $className');
+    } else {
+      print('에러 코드: ${response.statusCode}');
+    }
+  }
+
+  void _sendImageToServer() {
+    if (_imageFile != null) {
+      _uploadImage(_imageFile);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: _imageFile != null
+            ? Image.file(_imageFile)
+            : Text('No image selected'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () => _getImage(ImageSource.camera),
+            tooltip: 'Take a photo',
+            child: Icon(Icons.add_a_photo),
+          ),
+          SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () => _getImage(ImageSource.gallery),
+            tooltip: 'Pick an image',
+            child: Icon(Icons.photo_library),
+          ),
+          SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: _sendImageToServer,
+            tooltip: 'Send image to server',
+            child: Icon(Icons.cloud_upload),
+          ),
+        ],
+      ),
     );
   }
 }
