82d36ce3519b16939b6a1f479c2600008b397113
ldy <ldy8631@hanmail.net>
2023-05-24 21:28:31 +0900

    Drawer 이메일 아이디 표시 + 로그아웃

--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:petai_care/screens/LoginSignupScreen.dart';
 import 'package:petai_care/screens/account/account_screen.dart';
 import 'package:petai_care/screens/ai/ai_screen.dart';
 import 'package:petai_care/screens/board/board_screen.dart';
 import 'package:petai_care/screens/hospital/hospital_screen.dart';
 
 class MainScreens extends StatefulWidget {
-  const MainScreens({super.key});
+  const MainScreens({Key? key}) : super(key: key);
 
   @override
   _MainScreensState createState() => _MainScreensState();
@@ -16,6 +18,26 @@ class MainScreens extends StatefulWidget {
 class _MainScreensState extends State<MainScreens> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   int _selectedIndex = 0;
+  String? accountName;
+  String? accountEmail;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  Future<void> fetchUserData() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      await user.reload();
+      user = _auth.currentUser;
+      setState(() {
+        accountName = user?.displayName ?? '';
+        accountEmail = user?.email ?? '';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,8 +66,9 @@ class _MainScreensState extends State<MainScreens> {
               ),
               color: Colors.blue.shade200,
             ),
-            accountName: const Text('이름'),
-            accountEmail: const Text('이메일'),
+            accountName: Text(accountName ?? ''),
+            accountEmail: Text(accountEmail ?? '',
+            style: const TextStyle(fontSize: 20, color: Colors.black),),
             currentAccountPicture: const Icon(
               Icons.pets,
               size: 60,
@@ -60,8 +83,9 @@ class _MainScreensState extends State<MainScreens> {
           ListTile(
             title: const Text('로그아웃'),
             onTap: () {
-              Navigator.pop(context);
-            },
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const LoginSignupScreen()));
+                  },
           ),
         ]),
       ),
@@ -91,9 +115,9 @@ class _MainScreensState extends State<MainScreens> {
           BottomNavigationBarItem(
               label: '가계부', icon: Icon(CupertinoIcons.creditcard)),
           BottomNavigationBarItem(
-              label: '게시판', icon: Icon(CupertinoIcons.bubble_left)),
-        ],
-      ),
-    );
-  }
+              label: '게시판', icon:Icon(CupertinoIcons.bubble_left)),
+],
+),
+);
+}
 }
