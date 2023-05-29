2bdda87095032b3e9158e1a3c3a718fee08e92ed
ldy <ldy8631@hanmail.net>
2023-05-30 03:20:42 +0900

    펫 프로필 등록 화면 제작
    디바이스에 저장 / 파이어베이스에 연결해야 함

---
+++ lib/screens/diary/profile_controller.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+import 'package:get/get.dart';
+import 'package:petai_care/screens/diary/image_crop_controller.dart';
+import 'package:petai_care/screens/diary/user_model.dart';
+
+enum ProfileImageType { THUMBNAIL, BACKGROUND }
+
+class ProfileController extends GetxController {
+  RxBool isEditMyProfile = false.obs;
+  UserModel originMyProfile = UserModel(
+    name: "개발하는 남자",
+    discription: "구독과 좋아요",
+  );
+  Rx<UserModel> myProfile = UserModel().obs;
+
+  @override
+  void onInit() {
+    isEditMyProfile(false);
+    myProfile(UserModel.clone(originMyProfile));
+    super.onInit();
+  }
+
+  void toggleEditProfile() {
+    isEditMyProfile(!isEditMyProfile.value);
+  }
+
+  void rollback() {
+    myProfile.value.initImageFile();
+    myProfile(originMyProfile);
+    toggleEditProfile();
+  }
+
+  void updateName(String updateName) {
+    myProfile.update((my) {
+      my?.name = updateName;
+    });
+  }
+
+  void updateDiscription(String updateDiscription) {
+    myProfile.update((my) {
+      my?.discription = updateDiscription;
+    });
+  }
+
+  void pickImage(ProfileImageType type) async {
+    if (!isEditMyProfile.value) return;
+    File? file = await ImageCropController.to.selectImage();
+    if (file == null) return;
+    switch (type) {
+      case ProfileImageType.THUMBNAIL:
+        myProfile.update((my) => my?.avatarFile = file);
+        break;
+      case ProfileImageType.BACKGROUND:
+        myProfile.update((my) => my?.backgroundFile = file);
+        break;
+    }
+  }
+
+  void save(){
+    originMyProfile = myProfile.value;
+    toggleEditProfile();
+  }
+}
