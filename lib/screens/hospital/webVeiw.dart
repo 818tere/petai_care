7a35ab52a4823f8bca41bf03883eef14cbd9d217
ldy <ldy8631@hanmail.net>
2023-06-02 00:00:00 +0900

    병원정보(전화 걸기, 병원 정보 인앱 방식 네이버 이동)
    (병원 정보 페이지 이동은 첫 병원만 가능)

---
+++ lib/screens/hospital/webVeiw.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatelessWidget {
+  final String url;
+
+  const WebViewScreen({required this.url, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
