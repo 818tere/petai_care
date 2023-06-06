e5c7f8a90fc1e75b303d2275ccdd8c00ec28f1c6
818tere <a18@18ui-MacBookAir.local>
2023-06-06 22:29:24 +0900

    안드로이드 에뮬 위치설정 오류수정

--- android/app/src/main/kotlin/com/example/petai_care/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/petai_care/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.petai_care
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
 
 class MainActivity: FlutterActivity() {
      override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
